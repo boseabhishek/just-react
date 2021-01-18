4e09fc727ba18396e4bec09a1d2beda0f26122d6
Abhishek Bose <j2ee.abose@gmail.com>
2021-01-18 13:58:39 +0000

    minimal react impl

--- src/index.js
+++ src/index.js
@@ -1,81 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
+const formatName = (user) => user.firstName + ' ' + user.lastName;
 
-class Square extends React.Component {
-    constructor(props) {
-        {/* mandatory for all subclass contrsutors */ }
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: 'X' })}>
-                {/* same as onClick={function() { alert('click'); }} */}
-                {this.state.value}
-            </button>
-        );
-    }
-}
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
 
-class Board extends React.Component {
-
-    // this is a method
-    renderSquare(i) {
-        // passing a 'prop' named 'value'
-        return <Square value={i} />;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-// ========================================
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
 
 ReactDOM.render(
-    <Game />,
+    element,
     document.getElementById('root')
 );
